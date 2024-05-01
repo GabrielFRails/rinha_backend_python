4c5536de7381a59ce4ad1b0828e0e186278b5cfb
GabrielFRails <gabriel.estudy.reis@gmail.com>
2024-05-01 12:39:54 -0300

    Add endpoints skell, returning mocks for now

--- api/main.py
+++ api/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from api_models import *
 
 app = FastAPI()
 
@@ -7,4 +8,34 @@ def root_path():
 	return {
 		"msg": "Saudações ser pensante"
 	}
-	
+
+@app.post("/clientes/{id}/transacoes")
+def transaction_request(id: int, request: TransactionRequest):
+	return {
+		"limite" : 100000,
+		"saldo" : -9098
+	}
+
+@app.get("/clientes/{id}/extrato")
+def client_statement(id: int):
+	return {
+		"saldo": {
+			"total": -9098,
+			"data_extrato": "2024-01-17T02:34:41.217753Z",
+			"limite": 100000
+		},
+		"ultimas_transacoes": [
+			{
+				"valor": 10,
+				"tipo": "c",
+				"descricao": "descricao",
+				"realizada_em": "2024-01-17T02:34:38.543030Z"
+			},
+			{
+				"valor": 90000,
+				"tipo": "d",
+				"descricao": "descricao",
+				"realizada_em": "2024-01-17T02:34:38.543030Z"
+			}
+		]
+	}
