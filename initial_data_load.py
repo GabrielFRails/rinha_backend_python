809b50919228118904f70e160d89f1b723e823c5
GabrielFRails <gabriel.estudy.reis@gmail.com>
2024-05-13 17:52:35 -0300

    Create script to init db with some data

---
+++ initial_data_load.py
@@ -0,0 +1,44 @@
+import json
+import redis
+
+def main():
+# {
+	r = redis.Redis(host='redis', port=6379)
+
+	clients = [
+		{
+			"id": 1,
+			"limite": 100000,
+			"saldo": 0
+		},
+		{
+			"id": 2,
+			"limite": 80000,
+			"saldo": 0
+		},
+		{
+			"id": 3,
+			"limite": 1000000,
+			"saldo": 0
+		},
+		{
+			"id": 4,
+			"limite": 10000000,
+			"saldo": 0
+		},
+		{
+			"id": 5,
+			"limite": 500000,
+			"saldo": 0
+		}
+	]
+
+	if not r.keys(): # means empty db
+		for c in clients:
+			client_key = f"client:{c['id']}"
+			client_value = json.dumps(c)
+			r.set(client_key, client_value)
+# }
+
+if __name__ == "__main__":
+	main()
