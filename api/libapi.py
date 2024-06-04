c95b67f817d1d7390bdb48420a54091e9a668639
GabrielFRails <gabriel.estudy.reis@gmail.com>
2024-06-04 20:27:04 -0300

    Update libapi to use new libs to update client statement and get data compose last endpoint

--- api/libapi.py
+++ api/libapi.py
@@ -1,8 +1,11 @@
+import uuid
 from fastapi import HTTPException
 
 from api_models import *
 from libtransaction import *
+from libstatement import *
 from libclient import *
+from libutils import *
 
 def api_transaction(clientid, transaction):
 # {
@@ -16,6 +19,7 @@ def api_transaction(clientid, transaction):
 	log_info(f"transaction err: {err == True}")
 
 	if not err:
+		api_upate_statement(clientid, transaction)
 		return 0, {
 			"saldo": client_get_balance(clientid),
 			"limite": client_get_limit(clientid)
@@ -24,14 +28,32 @@ def api_transaction(clientid, transaction):
 	return 1, None, HTTPException(status_code=422, detail="Não foi possível realizar a transação")
 # }
 
+def api_upate_statement(clientid, transaction):
+# {
+	transaction_dict = transaction.__dict__
+	transaction_dict['tipo'] = transaction.tipo.value
+	transaction_dict['tid'] = str(uuid.uuid4())
+	statement_update(clientid, transaction_dict)
+# }
+
+def api_get_statement(clientid):
+# {
+	statement_db = statement_get(clientid)
+
+	data = []
+	for s in statement_db:
+		d = json.loads(s[0])
+		dt_formated = utils_convert_timestamp_to_datetime(s[1])
+		d['realizada_em'] = dt_formated
+		data.append(d)
+
+	return data
+# }
+
 def api_client_get(clientid):
 # {
 	if not check_client_existence(clientid):
 		return -1, None, HTTPException(status_code=404, detail="Cliente não encontrado na base de dados")  
 
-	return 0, {
-		"id": clientid,
-		"saldo": client_get_balance(clientid),
-		"limite": client_get_limit(clientid)
-    }, None
+	return 0, client_data_get(clientid), None
 # }
